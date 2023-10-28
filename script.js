6fc5364a2413abefceee6c81fec1afcc5c3cbdbc
ChandruMonica2705 <monicamahalingam27@gmail.com>
2023-10-28 16:56:56 +0530

    answers updated and need to update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//Q1.Comparing two JSON have the same properties without order:-
+// //Q1.Comparing two JSON have the same properties without order:-
 let obj1 = {name:"Person 1",age:5};
 let obj2 = {age:5,name:"Person 1"};
 console.log(JSON.stringify(obj1)===JSON.stringify(obj2));
-//Above program output is False
+// //Above program output is False
 
-//Q2.Displaying all countries flags in the given API:-
+// //Q2.Displaying all countries flags in the given API:-
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all",true)
 request.send();
@@ -17,7 +17,7 @@ request.onload = function(){
     
 }
 
-//Q2.Displaying all countries names,regions,sub region,population in the given API:-
+// //Q2.Displaying all countries names,regions,sub region,population in the given API:-
 var request1 = new XMLHttpRequest();
 request1.open("GET","https://restcountries.com/v3.1/all",true)
 request1.send();
@@ -29,3 +29,4 @@ request1.onload = function(){
         console.log(result1[i].name.common,",",result1[i].region,",",result1[i].subregion,",",result1[i].population);
     
 }
+
